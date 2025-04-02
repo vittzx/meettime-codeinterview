1cdd009144729d484c120a14a7e3b1c0fa5944c0
Vitor Cosso <vitorcosso@gmail.com>
2025-04-02 16:33:21 -0300

    first commit

---
+++ codeInterview/src/main/java/com/meet/time/interview/infra/adapters/input/ContactRestController.java
@@ -0,0 +1,7 @@
+package com.meet.time.interview.infra.adapters.input;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactRestController {
+}
---
+++ codeInterview/src/test/java/com/meet/time/interview/CodeInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meet.time.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
