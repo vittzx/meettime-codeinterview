72f2e47a72dcf57d8edfe569a144ef2205270096
Vitor Cosso <vitorcosso@gmail.com>
2025-04-02 17:51:10 -0300

    feat: creating HubspotPropertie

--- codeInterview/src/main/java/com/meet/time/interview/application/configuration/HubspotProperties.java
+++ codeInterview/src/main/java/com/meet/time/interview/application/configuration/HubspotProperties.java
@@ -1,4 +1,17 @@
 package com.meet.time.interview.application.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "application.hubspot-app")
 public class HubspotProperties {
+
+    private String clientSecret;
+    private String clientApp;
+    private String clientId;
+
 }
+
