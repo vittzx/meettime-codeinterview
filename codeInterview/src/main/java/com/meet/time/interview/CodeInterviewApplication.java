44e5b29cafa4222998241058eae5332854f4922e
Vitor Cosso <vitorcosso@gmail.com>
2025-04-02 17:52:05 -0300

    chore: adjusting MainClass configuration to HubSpot properties

--- codeInterview/src/main/java/com/meet/time/interview/CodeInterviewApplication.java
+++ codeInterview/src/main/java/com/meet/time/interview/CodeInterviewApplication.java
@@ -1,9 +1,12 @@
 package com.meet.time.interview;
 
+import com.meet.time.interview.application.configuration.HubspotProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({HubspotProperties.class})
 public class CodeInterviewApplication {
 
 	public static void main(String[] args) {
