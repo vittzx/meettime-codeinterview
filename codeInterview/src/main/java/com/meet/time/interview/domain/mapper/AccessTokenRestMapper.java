6ce12324954780dd78edaaeb9f51d538de628d3f
Vitor Cosso <vitorcosso@gmail.com>
2025-04-03 11:50:35 -0300

    feat: adding mapper

---
+++ codeInterview/src/main/java/com/meet/time/interview/domain/mapper/AccessTokenRestMapper.java
@@ -0,0 +1,4 @@
+package com.meet.time.interview.domain.mapper;
+
+public class AccessTokenRestMapper {
+}
---
+++ codeInterview/src/main/java/com/meet/time/interview/infra/adapters/input/data/response/AccessTokenResponseDTO.java
@@ -0,0 +1,4 @@
+package com.meet.time.interview.infra.adapters.input.data.response;
+
+public class AccessTokenResponseDTO {
+}
