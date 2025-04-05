2aa97d6df9a384299173f1f7a31db5a6c43d5af6
Vitor Cosso <vitorcosso@gmail.com>
2025-04-05 13:22:50 -0300

    config: adding api rate limits

--- codeInterview/src/main/java/com/meet/time/interview/CodeInterviewApplication.java
+++ codeInterview/src/main/java/com/meet/time/interview/CodeInterviewApplication.java
@@ -4,9 +4,11 @@ import com.meet.time.interview.application.configuration.HubspotProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableConfigurationProperties({HubspotProperties.class})
+@EnableCaching
 public class CodeInterviewApplication {
 
 	public static void main(String[] args) {
