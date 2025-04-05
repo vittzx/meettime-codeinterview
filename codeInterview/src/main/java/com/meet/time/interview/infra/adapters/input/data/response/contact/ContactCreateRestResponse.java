8b53e02aae466d6e59066d2ceb040b2ebee55b65
Vitor Cosso <vitorcosso@gmail.com>
2025-04-04 21:31:01 -0300

    feat: return contact created, Creating DTO's to response, adjusting return create contact use case and create mapper toContactCreatePropertiesRestResponse

--- codeInterview/src/main/java/com/meet/time/interview/infra/adapters/input/data/response/contact/ContactCreateRestResponse.java
+++ codeInterview/src/main/java/com/meet/time/interview/infra/adapters/input/data/response/contact/ContactCreateRestResponse.java
@@ -1,19 +1,4 @@
 package com.meet.time.interview.infra.adapters.input.data.response.contact;
 
-
-import lombok.*;
-
-@Builder
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class ContactCreateRestResponse {
-
-    private String id;
-    private String email;
-    private String fullName;
-    private String company;
-
 }
